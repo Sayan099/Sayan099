45ffa1c4ad938376fd8482d18514c59ca3bac85a
Sayan099 <145744448+Sayan099@users.noreply.github.com>
2023-12-05 16:43:38 +0530

    Update Add two numbers.c

--- Add two numbers.c
+++ Add two numbers.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-int main(){
-int a,b,c;
-printf("Enter two Numbers=\n");
-scanf("%d %d",&a,&b);
-c=a+6;
-printf("Addition of %d and %d is %d\n", a, b, c);
-return 0;
+int main() {    
+
+    int a, b, c;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &a,&b);
+    c = a+ b;      
+    printf("%d + %d = %d", a, b, c);
+    return 0;
+}
